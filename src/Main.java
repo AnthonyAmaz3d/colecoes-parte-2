f26f280dc6d829afae998b92e41a578310490c3c
Anthony Maia Dolberth <125688866+AnthonyAmaz3d@users.noreply.github.com>
2024-08-08 19:59:17 -0300

    alterando nome da variavel

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,12 @@ public class Main {
 
     for (int i = 0; i < quantasPessoas; i++) {
       String nome = Interface.definirNome();
-      String sexo = Interface.definirSexo();
+      String genero = Interface.definirSexo();
 
-      Pessoa pessoa = new Pessoa(nome, sexo);
-      if (sexo.equalsIgnoreCase("masculino")) {
+      Pessoa pessoa = new Pessoa(nome, genero);
+      if (genero.equalsIgnoreCase("masculino")) {
         pessoas.get(1).add(pessoa);
-      } else if (sexo.equalsIgnoreCase("feminino")) {
+      } else if (genero.equalsIgnoreCase("feminino")) {
         pessoas.get(2).add(pessoa);
       }
     }
@@ -30,12 +30,12 @@ public class Main {
 
   private static void imprimirPessoa(List<Pessoa> pessoas, String sexo) {
     if (pessoas != null && !pessoas.isEmpty()) {
-      System.out.println("Lista de pessoas do sexo: " + sexo);
+      System.out.println("Lista de pessoas do sexo: " + genero);
         for (Pessoa pessoa : pessoas) {
-            System.out.println("Nome: " + pessoa.getNome() + " Sexo: " + pessoa.getSexo());
+            System.out.println("Nome: " + pessoa.getNome() + " Genero: " + pessoa.getSexo());
         }
     } else {
-      System.out.println("Nenhuma pessoa do tipo: " + sexo);
+      System.out.println("Nenhuma pessoa do tipo: " + genero);
     }
   }
 }
